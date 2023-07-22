3a85406dd12adba60de2d423060eaef98bc535aa
kwangkyun kim <kyuni9786@naver.com>
2023-07-22 22:10:59 +0900

    라이엇 api 테이블 삭제

--- src/main/java/team2/spgg/domain/api/entity/EntryModel.java
+++ src/main/java/team2/spgg/domain/api/entity/EntryModel.java
@@ -7,37 +7,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "entrymodel")
 public class EntryModel {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+
 	private int id;
 
-	@Column
 	private String summonerName;
 
-	@Column
 	private String tier;
 
-	@Column(name = "rankName")
 	private String rank;
 
-	@Column
 	private long leaguePoints;
 
-	@Column
 	private int wins;
 
-	@Column
 	private int losses;
 
-	@Column
 	private double winRate;
 }
--- src/main/java/team2/spgg/domain/api/entity/RankingModel.java
+++ src/main/java/team2/spgg/domain/api/entity/RankingModel.java
@@ -7,29 +7,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "rankingmodel")
 public class RankingModel {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    
 	private int id;
-    @Column
+    
     private String summonerName;
-    @Column
+    
     private String tier;
-    @Column(name = "rankName")
+
     private String rank;
-    @Column
+    
     private long leaguePoints;
-    @Column
+    
     private int win;
-    @Column
+    
     private int lose;
 
 }
